447e1f6cbf3b9ed75e16cbde7d524db350d56203
ZacharyLukasikDrescher <94499946+ZacharyLukasikDrescher@users.noreply.github.com>
2021-11-16 19:07:04 -0700

    Update messageClass.java
    -Added getters and setters
    -Added third String receiveName and changed String name to String senderName to avoid any conflict that may arise when setMessageSender and setMessageRecipient are used

--- messageClass.java
+++ messageClass.java
@@ -1,4 +1,44 @@
 
 public class messageClass {
 
+  String messageSender;
+  String messageRecipient;
+  String messageContents;
+  
+  messageClass(String senderName, String receiveName, String message)
+  {
+    messageSender = senderName;
+    messageRecipient = receiveName;
+    messageContents = message;
+  }
+  
+  public String getMessageSender()
+  {
+    return messageSender;
+  }
+  
+  public String getMessageRecipient()
+  {
+    return messageRecipient;
+  }
+  
+  public String getMessageContents()
+  {
+    return messageContents;
+  }
+  
+  public void setMessageSender()
+  {
+    messageSender = senderName;
+  }
+  
+  public void setMessageRecipient()
+  {
+    messageRecipient = receiveName;
+  }
+  
+  public void setMessageContents()
+  {
+    messageContents = message;
+  }
 }
